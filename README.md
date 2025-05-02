af725b9980c80cfa4a7aab0a249a392c938d912e
David Schneider <david@ingeniouslycrazy.com>
2025-05-02 16:22:28 +0200

    Removing the charts

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ A dockerized k8s sandbox. Features:
 - Cert-Manager
 - Metrics server
 - Gitlab Operator
+
+## Deploy
+
+1. Run './bin/create-cluster.sh' to create the cluster.
+2. Run './bin/deploy-gitlab.sh' to deploy a Gitlab installation.
+
+## Purge
+
+Run './bin/purge-cluster.sh' to purge the cluster.
+
+## Working offline
+
+docker pull kindest/node:v1.31.2
---
+++ bin/deploy-gitlab.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install gitlab-operator
+kubectl create namespace gitlab-system
+helm upgrade --install gitlab-operator gitlab/gitlab-operator --namespace gitlab-system
+# Wait until dust has settled
+kubectl -n gitlab-system wait --for=condition=Ready -l 'control-plane=controller-manager' pods --timeout=300s
+
+# Install GitLab
+kubectl -n gitlab-system apply -f $(pwd)/cluster/gitlab/gitlab.yml
+# Wait until dust has settled
+kubectl -n gitlab-system wait gitlab gitlab --for=condition=available --timeout 600s
+
+# Get the password to configure GitLab
+kubectl -n gitlab-system get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
